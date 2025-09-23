c692bfef7caaa54b873ee59d0a2ec662b41a50a7
mohsen <m16192494@gmail.com>
2025-09-23 10:08:23 -0700

    add a new view to show recent transactions

--- expenses/urls.py
+++ expenses/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
 from . import views
-urlpatterns=[path("",views.index,name="index"),path("transactions/",views.show_transactions,name="show_transactions"),]
+urlpatterns=[path("",views.index,name="index"),
+             path("transactions/",views.show_transactions,name="show_transactions"),
+             path("recent/<int:days>",views.show_recent_transactions,name="recent")
+             ]
--- expenses/views.py
+++ expenses/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Income,Outlay
+import datetime
+from django.utils import timezone
 
 def index(request):
     incomes = Income.objects.all()          
-    return HttpResponse(incomes)
+    return HttpResponse(request.user)
 def show_transactions(request):
     incomes = Income.objects.all()
     outlays = Outlay.objects.all()
@@ -13,5 +15,13 @@ def show_transactions(request):
         'outlays': outlays
     }
     return render(request, 'expenses/transactions.html', context)
+def show_recent_transactions(request,days):
+    start_date=timezone.now()-datetime.timedelta(days=days)
+    recent_incomes=Income.objects.filter(date__gte=start_date)
+    recent_outlays=Outlay.objects.filter(date__gte=start_date)
+    contex={"recent_incomes":recent_incomes,"recent_outlays":recent_outlays,"days":days}
+    return render(request,"expenses/recent.html",contex)
+
+    
 
 # Create your views here.
